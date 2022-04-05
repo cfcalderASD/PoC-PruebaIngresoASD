0a8277ec8a4df18cb693ae2935e3d3b98c2c5ced
cfcalder <ccalderonr@grupoasd.com.co>
2022-04-05 11:46:32 -0500

    sql configuration

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO persona_entity (id, nombre, cedula) VALUES (1, 'Carlos', '1090469286');
-INSERT INTO persona_entity (id, nombre, cedula) VALUES (2, 'Fernando', '1090469351');
-INSERT INTO area_entity (id, ciudad) VALUES (1, 'Cucuta');
-INSERT INTO area_entity (id, ciudad) VALUES (2, 'Bogota');
+
+
+INSERT INTO persona_entity (id, nombre, cedula) VALUES (1, 'Carlos', '1090469286'),
+    (2, 'Fernando', '1090469351');
+INSERT INTO area_entity (id, ciudad) VALUES (1, 'Cucuta'), (2, 'Bogota');
